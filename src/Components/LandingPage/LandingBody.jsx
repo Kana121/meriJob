0115ec9b69006f1cf5498d0d95184f32f76356b6
Yeshwanth Kondaveeti <105161096+Yeshwanth999@users.noreply.github.com>
2023-12-07 17:53:52 +0530

    LandingBody search bar State suggestion changes

--- src/Components/LandingPage/LandingBody.jsx
+++ src/Components/LandingPage/LandingBody.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import style from "./landingbody.module.css"
 import JobList from './JobList'
 import LandingPageBanner from './LandingPageBanner'
 import Faqs from '../FAQs/Faqs'
-import statesAndCitiesIndia from  '../Assets/SatesCities'
+import Countries from '../Assets/countries.json'
+import States from '../Assets/states.json'
+import Cities from '../Assets/cities.json'
+
+
+
 
 const LandingBody = ({changealltohide}) => {
+  const countries = Countries;
+  const states = States;
+  const cities = Cities;
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [filteredStates, setFilteredStates] = useState([]);
+  const [inputText, setInputText] = useState('');
   
- 
+  const handleCountryChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedCountry(selectedValue);
+    setFilteredStates([]); // Reset filtered states when country changes
+  };
+  const handleInputTextChange = (event) => {
+    const text = event.target.value;
+    setInputText(text);
+
+    // Filter states based on the selected country and input text
+    const countryStates = states.filter(state => state.country_name === selectedCountry);
+    const filteredStates = countryStates.filter(state =>
+      state.name.toLowerCase().startsWith(text.toLowerCase())
+    );
+    setFilteredStates(filteredStates);
+    ToggleSuggestionListDisplay();    
+  };
+  function ToggleSuggestionListDisplay() {
+    const stateSuggestionList = document.getElementById("stateSuggestionList");
+    if(document.getElementById("bxStateSearch").value !==""){
+      if(stateSuggestionList){
+        stateSuggestionList.style.display="block";
+        
+      }
+      else{
+        stateSuggestionList.style.display="none";
+      }
+    }
+    else{
+      stateSuggestionList.style.display="none";
+    }
+    
+  }
+    
+   
   return (
     <div onMouseEnter={changealltohide} className={style.lbody}>
       <LandingPageBanner/>
@@ -30,7 +75,27 @@ const LandingBody = ({changealltohide}) => {
                 <option value="">6 Year</option>
                 <option value="">7 Year</option>
               </select>
-              <input type="text" placeholder='Enter location' className={style.lbinp2} />
+              <select value={selectedCountry} onChange={handleCountryChange} className={style.lbinp2}>
+                <option value="" disabled selected>
+                Select Country
+                </option>
+                {countries.map((country) => (
+                  <option key={country.id} value={country.name}>
+                    {`${country.name}`}
+                  </option>
+                ))}
+              </select>
+              <div id="stateNcity" className={style.stateNcity}>
+              <input type="text" id="bxStateSearch" placeholder='Enter location' className={style.lbinp2} value={inputText} onChange={handleInputTextChange}/>
+              <ul id="stateSuggestionList" className={style.stateSuggestionList}>
+                {filteredStates.map(state => (
+                  <li key={state.id} className={style.listitem}>
+                    {state.name}
+                  </li>
+                ))}
+              </ul>
+              
+              </div>
               <button className={style.lbbtn1}>Search</button>
             </div>
         </div>
